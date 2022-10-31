a6b6a99dc93f2de5b19467cd154a32b9305de97f
Noah Kim <dongwoo_333@hanmail.net>
2022-11-01 10:01:45 +1100

    93. Running Server-side Code & Using the Filesystem

--- 05-data-fetching/pages/index.js
+++ 05-data-fetching/pages/index.js
@@ -1,3 +1,5 @@
+import path from "path";
+
 // run second
 function HomePage(props) {
   const { products } = props;
@@ -13,13 +15,15 @@ function HomePage(props) {
 
 // run first: consider it that it's not on front side
 export async function getStaticProps() {
+  const fs = require("fs").promises;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // current working directory, 'root'
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [
-        { id: "p1", title: "Product 1" },
-        { id: "p2", title: "Product 2" },
-        { id: "p3", title: "Product 3" },
-      ],
+      products: data.products,
     },
   };
 }
