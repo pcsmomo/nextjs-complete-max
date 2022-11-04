413044799bc1b3cec91b188ba2ba167e7c003a67
Noah Kim <dongwoo_333@hanmail.net>
2022-11-04 18:30:26 +1100

    128. Analyzing the Need for "head" Metadata"

---
+++ 07-prj-optimizations/pages/events/[...slug].js
@@ -0,0 +1,139 @@
+import { Fragment, useEffect } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+// import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+
+function FilteredEventsPage(props) {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const fetcher = (...args) =>
+    fetch(...args)
+      .then((res) => res.json())
+      .then((data) =>
+        Object.keys(data).map((key) => ({
+          ...data[key],
+          id: key,
+        }))
+      );
+
+  const { data: loadedEvents, error } = useSWR(
+    "https://nextjs-course-f4041-default-rtdb.firebaseio.com/events.json",
+    fetcher
+  );
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [filteredYear, filteredMonth] = filterData;
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    // if (props.hasError) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
+  // const filteredEvents = props.events;
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+
+//   const filterData = params.slug;
+
+//   const [filteredYear, filteredMonth] = filterData;
+
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
+
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     return {
+//       props: { hasError: true },
+//       // notFound: true,
+//       // redirect: {
+//       //   destination: '/error'
+//       // }
+//     };
+//   }
+
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
+
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   };
+// }
+
+export default FilteredEventsPage;
---
+++ 07-prj-optimizations/pages/events/index.js
@@ -0,0 +1,38 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+// import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
+
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
