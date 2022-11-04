88cc8401d853a62d6b4ff3e10bdc45d10b228d78
Noah Kim <dongwoo_333@hanmail.net>
2022-11-04 19:45:16 +1100

    144. Parsing The Incoming Request & Executing Server-side Code

--- 08-api-routes/pages/api/feedback.js
+++ 08-api-routes/pages/api/feedback.js
@@ -1,5 +1,34 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      res.status(200).json({ message: "This works!" });
+      break;
+    case "POST":
+      const { email, feedback } = req.body;
+      const newFeedback = {
+        id: new Date().toISOString(),
+        email,
+        text: feedback,
+      };
+
+      // store that in a database or in a file
+      const filePath = path.join(process.cwd(), "data", "feedback.json");
+      const fileData = fs.readFileSync(filePath);
+      const data = JSON.parse(fileData);
+      data.push(newFeedback);
+      fs.writeFileSync(filePath, JSON.stringify(data));
+
+      res.status(201).json({ message: "Success!", feedback: newFeedback });
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "PUT"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
 
 export default handler;
