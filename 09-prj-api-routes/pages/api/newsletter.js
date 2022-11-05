682738a44544877b5c2a8eea4b56966eafa8eb69
Noah Kim <dongwoo_333@hanmail.net>
2022-11-05 19:15:35 +1100

    158. Running MongoDB Queries From Inside API Routes

--- 09-prj-api-routes/pages/api/newsletter.js
+++ 09-prj-api-routes/pages/api/newsletter.js
@@ -1,10 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import { mongodbUrl } from "../../config/env";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
     if (!userEmail || !userEmail.includes("@")) {
       return res.status(422).json({ message: "Invalid email address." });
     }
+
+    const client = await MongoClient.connect(mongodbUrl);
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
 
     console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
