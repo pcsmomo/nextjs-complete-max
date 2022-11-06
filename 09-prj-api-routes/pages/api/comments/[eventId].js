73a8a88cee9558e9b9f6c02fcd75c89a65dc7a17
Noah Kim <dongwoo_333@hanmail.net>
2022-11-07 08:23:57 +1100

    163. More Error Handling

--- 09-prj-api-routes/pages/api/comments/[eventId].js
+++ 09-prj-api-routes/pages/api/comments/[eventId].js
@@ -1,5 +1,8 @@
-import { MongoClient } from "mongodb";
-import { MONGODB_URL } from "../../../config/env";
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from "../../../helpers/db-util";
 
 async function handler(req, res) {
   const {
@@ -7,7 +10,13 @@ async function handler(req, res) {
     query: { eventId },
   } = req;
 
-  const client = await MongoClient.connect(MONGODB_URL);
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Connecting to the database failed" });
+    return;
+  }
 
   switch (method) {
     case "POST": {
@@ -21,7 +30,9 @@ async function handler(req, res) {
         !text ||
         text.trim() === ""
       ) {
-        return res.status(422).json({ message: "Invalid input" });
+        res.status(422).json({ message: "Invalid input" });
+        client.close();
+        return;
       }
 
       const newComment = {
@@ -31,32 +42,35 @@ async function handler(req, res) {
         eventId,
       };
 
-      const db = client.db();
-
-      const result = await db.collection("comments").insertOne(newComment);
-
-      console.log(result);
-
-      newComment.id = result.insertedId;
-
-      return res
-        .status(201)
-        .json({ message: "Added comment", comment: newComment });
+      let result;
+      try {
+        result = await insertDocument(client, "comments", newComment);
+        newComment._id = result.insertedId;
+        return res
+          .status(201)
+          .json({ message: "Added comment", comment: newComment });
+      } catch (error) {
+        res.status(500).json({ message: "Inserting comment failed" });
+      }
     }
     case "GET": {
-      const db = client.db();
-
-      const documents = await db
-        .collection("comments")
-        .find()
-        .sort({ _id: -1 })
-        .toArray();
-
-      return res.status(200).json({ comments: documents });
+      try {
+        const documents = await getAllDocuments(
+          client,
+          "comments",
+          {
+            _id: -1,
+          },
+          { eventId: eventId }
+        );
+        res.status(200).json({ comments: documents });
+      } catch (error) {
+        res.status(500).json({ message: "Getting comments failed." });
+      }
     }
     default:
-      res.setHeader("Allow", ["GET", "PUT"]);
-      return res.status(405).end(`Method ${method} Not Allowed`);
+    // res.setHeader("Allow", ["GET", "PUT"]);
+    // res.status(405).end(`Method ${method} Not Allowed`);
   }
 
   client.close();
