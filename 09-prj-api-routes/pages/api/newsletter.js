5b3ab7778a4dbc1f0f0b67f21399c350c71703c9
Noah Kim <dongwoo_333@hanmail.net>
2022-11-07 08:08:33 +1100

    162. Adding Error Handling

--- 09-prj-api-routes/pages/api/newsletter.js
+++ 09-prj-api-routes/pages/api/newsletter.js
@@ -1,6 +1,17 @@
 import { MongoClient } from "mongodb";
 import { MONGODB_URL } from "../../config/env";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(MONGODB_URL);
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -9,12 +20,22 @@ async function handler(req, res) {
       return res.status(422).json({ message: "Invalid email address." });
     }
 
-    const client = await MongoClient.connect(MONGODB_URL);
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
 
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
 
     console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
