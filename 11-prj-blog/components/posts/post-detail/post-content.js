46646dae925d98cf343cc05c4bc7d54dec2dc88b
Noah Kim <dongwoo_333@hanmail.net>
2022-11-11 16:45:54 +1100

    192. Rendering Dynamic Post Pages & Paths

--- 11-prj-blog/components/posts/post-detail/post-content.js
+++ 11-prj-blog/components/posts/post-detail/post-content.js
@@ -1,28 +1,18 @@
 import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
-
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-next",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-11-10",
-  content: `
-# This is a first post
-### this is a markdown
-`,
-};
+function PostContent(props) {
+  const { post } = props;
 
-function PostContent() {
   // const imagePath = `/images/posts/${slug}/${image}`;
   const imagePath = `/images/site/noah.png`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
