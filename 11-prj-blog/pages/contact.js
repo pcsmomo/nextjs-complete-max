b926c217f7da311333e1001679d1e951275b69db
Noah Kim <dongwoo_333@hanmail.net>
2022-11-13 10:01:50 +1100

    196. Preparing The Contact Form

--- 11-prj-blog/pages/contact.js
+++ 11-prj-blog/pages/contact.js
@@ -1,7 +1,10 @@
+import ContactForm from "../components/contact/contact-form";
+
 function ContactPage() {
   return (
     <div>
       <h1>Contact Page</h1>
+      <ContactForm />
     </div>
   );
 }
