0d31c5985f968c0353a28d8615f2d2da7345ff30
Noah Kim <dongwoo_333@hanmail.net>
2022-11-14 07:03:21 +1100

    208. Key Deployment Steps

---
+++ 12-prj-blog-deployment/pages/api/contact.js
@@ -0,0 +1,55 @@
+import { MongoClient } from "mongodb";
+import { MONGODB_URL } from "../../config/env";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    let client;
+    try {
+      client = await MongoClient.connect(MONGODB_URL);
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    let result;
+    try {
+      result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
+}
+
+export default handler;
