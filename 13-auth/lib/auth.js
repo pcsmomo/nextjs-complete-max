c7e06c9672b91080aa6d49cce9229fb946b58752
Noah Kim <dongwoo_333@hanmail.net>
2022-11-15 17:41:36 +1100

    224. Improving Signup With Unique Email Addresses

--- 13-auth/lib/auth.js
+++ 13-auth/lib/auth.js
@@ -2,5 +2,5 @@ import { hash } from "bcryptjs";
 
 export async function hashPassword(password) {
   const hashedPassword = await hash(password, 12); // for salt number, the higher the more secure
-  return hashPassword;
+  return hashedPassword;
 }
