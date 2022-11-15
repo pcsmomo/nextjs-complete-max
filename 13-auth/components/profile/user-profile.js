4ba6519a4ef2cecf90a0a9a1ef7c809f2dc09734
Noah Kim <dongwoo_333@hanmail.net>
2022-11-16 08:30:21 +1100

    229. Adding Client-Side Page Guards (Route Protection)

--- 13-auth/components/profile/user-profile.js
+++ 13-auth/components/profile/user-profile.js
@@ -1,8 +1,46 @@
+import { useState, useEffect } from "react";
+import { useSession, getSession } from "next-auth/react";
+
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  // Alternate way: using getSession
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [loadedSession, setLoadedSession] = useState();
+
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     setLoadedSession(session);
+  //     setIsLoading(false);
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     }
+  //   });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
+
+  // const [session, loading] = useSession(); // v3
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
+  if (!session) {
+    window.location.href = "/auth";
+    return <p className={classes.profile}>Checking...</p>;
+  }
+
+  useEffect(() => {
+    if (!session) {
+      window.location.href = "/auth";
+    }
+  }, [session]);
+
+  if (loading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
