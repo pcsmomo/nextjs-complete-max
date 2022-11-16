09f02cad79b390e6fe92ec9ed8bc8449b90357b2
Noah Kim <dongwoo_333@hanmail.net>
2022-11-17 07:38:09 +1100

    235. Adding the "Change Password" Logic

--- 13-auth/pages/user/change-password.js
+++ 13-auth/pages/user/change-password.js
@@ -1,5 +1,8 @@
 import { getSession } from "next-auth/react";
 
+import { connectToDatabase } from "../../lib/db";
+import { verifyPassword, hashPassword } from "../../lib/auth";
+
 async function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
@@ -11,6 +14,43 @@ async function handler(req, res) {
     res.status(401).json({ message: "Not authenticated!" });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.old - password;
+  const newPassword = req.body.new - password;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection("users");
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: "Invalid password." });
+    // res.status(422).json({ message: "User not found." });  // 422 Unprocessable Entity is also fine
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated!" });
 }
 
 export default handler;
